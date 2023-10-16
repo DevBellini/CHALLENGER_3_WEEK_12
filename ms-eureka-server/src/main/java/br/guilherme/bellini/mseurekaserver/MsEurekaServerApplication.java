0be88cfb80659405e1c37ed0b1c56dfc9bc92a62
Guilherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-16 11:30:25 -0300

    feat: application.yml configuration

--- ms-eureka-server/src/main/java/br/guilherme/bellini/mseurekaserver/MsEurekaServerApplication.java
+++ ms-eureka-server/src/main/java/br/guilherme/bellini/mseurekaserver/MsEurekaServerApplication.java
@@ -2,7 +2,9 @@ package br.guilherme.bellini.mseurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServergit 
 @SpringBootApplication
 public class MsEurekaServerApplication {
 
