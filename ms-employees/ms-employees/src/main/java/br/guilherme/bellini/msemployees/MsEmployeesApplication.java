0cbf133cc817623395562a3b6f72ca0394aecd1c
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-18 13:15:46 -0300

    init: ms-employees

---
+++ ms-employees/ms-employees/src/main/java/br/guilherme/bellini/msemployees/MsEmployeesApplication.java
@@ -0,0 +1,13 @@
+package br.guilherme.bellini.msemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmployeesApplication.class, args);
+	}
+
+}
