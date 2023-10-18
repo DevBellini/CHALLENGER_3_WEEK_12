43c7df697d3fe2f470bc7387b6b987f5bb67eabb
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-18 15:42:52 -0300

    init: ms-proposal

---
+++ ms-proposal/src/main/java/br/guilherme/bellini/msproposal/MsProposalApplication.java
@@ -0,0 +1,13 @@
+package br.guilherme.bellini.msproposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProposalApplication.class, args);
+	}
+
+}
