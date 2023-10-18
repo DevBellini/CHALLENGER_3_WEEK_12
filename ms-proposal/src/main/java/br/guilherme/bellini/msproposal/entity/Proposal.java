f293e8a2403f87e0be7bb28216525c9bd63aab08
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-18 15:52:27 -0300

    feat: proposal entity

---
+++ ms-proposal/src/main/java/br/guilherme/bellini/msproposal/entity/Proposal.java
@@ -0,0 +1,32 @@
+package br.guilherme.bellini.msproposal.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang.builder.EqualsBuilder;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "tb_proposal")
+public class Proposal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String Proposal;
+
+    public Proposal(Long id, String proposal) {
+        this.id = id;
+        Proposal = proposal;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(obj, this);
+    }
+
+}
+
+
