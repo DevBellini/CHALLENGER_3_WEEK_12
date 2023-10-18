548fe0523656b35896360cb8ca162acdfa4a0b52
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-18 14:00:02 -0300

    feat: yml configuration

--- ms-employees/src/main/java/br/guilherme/bellini/msemployees/MsEmployeesApplication.java
+++ ms-employees/src/main/java/br/guilherme/bellini/msemployees/MsEmployeesApplication.java
@@ -2,7 +2,9 @@ package br.guilherme.bellini.msemployees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsEmployeesApplication {
 
