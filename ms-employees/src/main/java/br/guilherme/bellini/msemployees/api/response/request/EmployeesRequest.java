afbf23eef9f39f89f4e99f98975f787c3635a431
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-18 14:32:28 -0300

    feat: EmployeesRequest

---
+++ ms-employees/src/main/java/br/guilherme/bellini/msemployees/api/response/request/EmployeesRequest.java
@@ -0,0 +1,20 @@
+package br.guilherme.bellini.msemployees.api.response.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeesRequest {
+
+    @NotBlank(message = "name is required")
+    private String name;
+    @NotBlank(message = "cpf is required")
+    private String cpf;
+    @NotNull(message = "age is required")
+    private Integer age;
+}
