20e43c09a48b1023c37b1c2251f5d3bbb1422e4b
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-18 17:05:38 -0300

    feat: employees route implementation

--- ms-gateway/src/main/java/br/guilherme/bellini/msgateway/MsGatewayApplication.java
+++ ms-gateway/src/main/java/br/guilherme/bellini/msgateway/MsGatewayApplication.java
@@ -3,13 +3,26 @@ package br.guilherme.bellini.msgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MsGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsGatewayApplication.class, args);
+    }
 
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/employees/**").uri("lb://ms-employees"))
+                
+                .build();
+
+    }
 }
