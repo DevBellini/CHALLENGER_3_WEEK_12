1f70e886d6960e911b5b08daddd5254ffceac5de
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-20 14:27:58 -0300

    feat: vote repository

---
+++ ms-votes/ms-votes/src/main/java/br/guilherme/bellini/msvotes/domain/repository/VoteRepository.java
@@ -0,0 +1,9 @@
+package br.guilherme.bellini.msvotes.domain.repository;
+
+import br.guilherme.bellini.msvotes.domain.entity.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+
+}
