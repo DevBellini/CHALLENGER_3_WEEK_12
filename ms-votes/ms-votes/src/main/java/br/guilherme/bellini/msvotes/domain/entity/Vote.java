50ee6977a625487777789b4982a9d83f8a403086
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-20 09:45:36 -0300

    feat: vote entity

---
+++ ms-votes/ms-votes/src/main/java/br/guilherme/bellini/msvotes/domain/entity/Vote.java
@@ -0,0 +1,20 @@
+package br.guilherme.bellini.msvotes.domain.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String employeesCpf;
+    private Long proposalId;
+    
+}
